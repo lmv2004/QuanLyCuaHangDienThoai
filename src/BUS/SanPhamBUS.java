c3cb6b91c123a1ada4ffb0a26d6214cc6b973f38
NguyenHoang-pluto <152002439+NguyenHoang-pluto@users.noreply.github.com>
2024-05-05 18:48:30 +0700

    Update SanPhamBUS.java

--- src/BUS/SanPhamBUS.java
+++ src/BUS/SanPhamBUS.java
@@ -8,13 +8,19 @@ public class SanPhamBUS {
     private final SanPhamDAO SPDAO = new SanPhamDAO();
     private final PhienBanSanPhamBUS pbspBUS = new PhienBanSanPhamBUS();
     private ArrayList<SanPhamDTO> listSp = new ArrayList<>();
-    
+
     public SanPhamBUS() {
         listSp = SPDAO.selecAll();
+    }
+    public SanPhamDTO getByIndex(int index) {
+        return this.listSp.get(index);
     }
 
     public ArrayList<SanPhamDTO> getAll() {
         return this.listSp;
+    }
+     public static SanPhamBUS getInstance() {
+        return new SanPhamBUS();
     }
 
     public ArrayList<SanPhamDTO> getAllSanPhamAttribute() {
@@ -69,16 +75,55 @@ public class SanPhamBUS {
         
         return result;
     }
-    public ArrayList<SanPhamDTO> search(String text){
-        ArrayList<SanPhamDTO> ketqua=new ArrayList<>();
-        text=text.toLowerCase();
-        for(SanPhamDTO sp:SPDAO.getAllSanPhamFullThongTin()){
-            if(sp.getTensp().toLowerCase().contains(text)){
-                //System.out.println(sp);
-                ketqua.add(sp);
+
+    public ArrayList<SanPhamDTO> search(String type, String txt) {
+    ArrayList<SanPhamDTO> result = new ArrayList<>();
+    txt = txt.toLowerCase();
+    switch (type) {
+        case "Tất cả":
+            for (SanPhamDTO i : listSp) {
+                if (Integer.toString(i.getMasp()).contains(txt) ||
+                    i.getTensp().toLowerCase().contains(txt) ||
+                     Integer.toString(i.getXuatxu()).contains(txt) ||
+                    i.getChipxuly().toLowerCase().contains(txt) ||
+                    Integer.toString(i.getDungluongpin()).contains(txt) ||
+                    Double.toString(i.getKichthuocman()).contains(txt) ||
+                    Integer.toString(i.getHedieuhanh()).contains(txt) ||
+                    Integer.toString(i.getPhienbanhdh()).contains(txt) ||
+                    i.getCamerasau().toLowerCase().contains(txt) ||
+                    i.getCameratruoc().toLowerCase().contains(txt) ||
+                    Integer.toString(i.getThoigianbaohanh()).toLowerCase().contains(txt) ||
+
+                  Integer.toString(i.getThuonghieu()).contains(txt) ||
+                  Integer.toString(i.getKhuvuckho()).contains(txt) ||
+                    Integer.toString(i.getSoluongton()).contains(txt)) {
+                    result.add(i);
+                }
             }
-        }
-        return ketqua;
+            break;
+
+        case "Mã sản phẩm":
+            for (SanPhamDTO i : listSp) {
+                if (Integer.toString(i.getMasp()).contains(txt)) {
+                    result.add(i);
+                }
+            }
+            break;
+
+        case "Tên sản phẩm":
+            for (SanPhamDTO i : listSp) {
+                if (i.getTensp().toLowerCase().contains(txt)) {
+                    result.add(i);
+                }
+            }
+            break;
+
+        // Thêm các trường tìm kiếm khác nếu cần
+
+        default:
+            throw new AssertionError();
     }
-    
+    return result;
+}
+
 }
